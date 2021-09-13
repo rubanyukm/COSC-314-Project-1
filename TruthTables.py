7625a1dfb893a09d8fe47bc7f148c76e4908e961
Ruben Maidaniuc <50090403+rubanyukm@users.noreply.github.com>
2021-09-13 18:26:50 -0400

    Update TruthTables.py

--- TruthTables.py
+++ TruthTables.py
@@ -1,6 +1,7 @@
 
 import ttg
 
+#Prints a table for the truth values of p and q
 table = ttg.Truths(['p', 'q'] , ['p and q', 'p or q', 'p xor q', 'p = q'], ints=False)
 
 print(table.as_prettytable())
